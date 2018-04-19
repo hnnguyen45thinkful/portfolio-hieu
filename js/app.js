22ac0fce6ea884768c1f79e186c8e24565f99a2d
Hieu Nguyen <hnnguyen45thinkful@users.noreply.github.com>
2018-04-19 11:33:23 -0700

    make the whole portfolio responsive and styling

--- js/app.js
+++ js/app.js
@@ -1,4 +1,33 @@
 $(document).ready(function() {
+  $(window).scroll(function(){
+    if($('html, body').scrollTop()>50){
+      $('.navbar').addClass('have_shadow');
+    }else{
+      $('.navbar').removeClass('have_shadow');
+    }
+  });
+
+  var menu_status=false;
+  $('.btn_mobile_menu').click(function(){
+    if(menu_status==false){
+      $('.navbar').addClass('show_menu');
+      $('.btn_mobile_menu').css('left', '190px');
+      menu_status=true;
+    }else{
+      $('.navbar').removeClass('show_menu');
+      $('.btn_mobile_menu').css('left', '0');
+      menu_status=false;
+    }
+  });
+
+  $('.navbar ul li a').click(function(){
+    if($(window).width()<=768){
+      $('.navbar').removeClass('show_menu');
+      $('.btn_mobile_menu').css('left', '0');
+      menu_status=false;
+    }
+  });
+
   // $('#contact-form form').submit(function(e) {
   //    e.preventDefault();
   //   var email   = $('#inputEmail').val();
